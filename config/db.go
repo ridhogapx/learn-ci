d0ff69bb84a248eba4c9f323078d69f182e621e4
RageNeko26 <rneko2006@gmail.com>
2023-08-21 01:54:48 +0700

    fix: change build

--- config/db.go
+++ config/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func NewDBConnection() *gorm.DB {
-	dsn := "host=172.17.0.3 user=root password=root dbname=memo port=5432 sslmode=disable"
+	dsn := "host=10.10.10.3 user=root password=root dbname=memo port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
