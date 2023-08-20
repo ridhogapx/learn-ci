924ed32aaa803b7b9ecf1e5a680f38380e9126ac
RageNeko26 <rneko2006@gmail.com>
2023-08-21 02:44:31 +0700

    feat: mocking repository and test it in service

--- service/memo_service_test.go
+++ service/memo_service_test.go
@@ -1,7 +1,31 @@
 package service
 
-import "testing"
+import (
+	"learn-ci/entity"
+	"learn-ci/model"
+	"learn-ci/repository"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var memoRepository = repository.MemoRepositoryMock{Mock: mock.Mock{}}
+var memoService = memoServiceImpl{Repository: &memoRepository}
 
 func TestMemoService(t *testing.T) {
+	expect := entity.MemoEntity{
+		Title:  "My Activity",
+		Body:   "Slept",
+		Author: "rneko2006",
+	}
 
+	memoRepository.Mock.On("Read", "rneko2006").Return(expect)
+
+	res, err := memoService.GetMemo(model.GetMemoRequest{
+		Author: "rneko2006",
+	})
+
+	assert.Nil(t, err)
+	assert.Equal(t, expect.Title, res.Title)
 }
