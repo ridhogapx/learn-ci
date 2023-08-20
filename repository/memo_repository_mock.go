f6be856eb6ff0a9dae12501ffec1f69f40c556d3
RageNeko26 <rneko2006@gmail.com>
2023-08-21 01:18:14 +0700

    fea: Mocking object

---
+++ repository/memo_repository_mock.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"errors"
+	"learn-ci/entity"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type MemoRepositoryMock struct {
+	Mock mock.Mock
+}
+
+func (repository *MemoRepositoryMock) Save(req *entity.MemoEntity) error {
+	arg := repository.Mock.Called(req)
+
+	if arg.Get(0) == nil {
+		return nil
+	}
+
+	return arg.Error(0)
+
+}
+
+func (repository *MemoRepositoryMock) Read(author string) (*entity.MemoEntity, error) {
+	arg := repository.Mock.Called(author)
+
+	if arg.Get(0) == nil {
+		return nil, errors.New("Not found")
+	}
+
+	return arg.Get(0).(*entity.MemoEntity), nil
+}
